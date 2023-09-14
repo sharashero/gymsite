461f4e2174b0f30101788e3dc530d82f267f1a3e
sharashero <sharshar.dev@gmail.com>
2023-09-15 00:10:28 +0300

    added PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Gymsite",
+        short_name: "Gymsite",
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#000",
+        background_color: "#fff",
+        description: "A gym management app.",
+        icons: [
+          {
+            src: "images/logos/gym-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "images/logos/gym-64.png",
+            sizes: "64x64",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
 });
