70b7356e9122642d9ec832d35280d154177f5b41
sharashero <sharshar.dev@gmail.com>
2023-09-15 18:09:36 +0300

    added base document types for CRUD

--- src/firebase/firestore/types/common.ts
+++ src/firebase/firestore/types/common.ts
@@ -2,3 +2,24 @@ export type TDocument = {
   id?: string;
   [key: string]: unknown;
 }
+
+
+type TConstructor = {
+  new (): unknown;
+}
+
+
+type TBaseObject = {
+  [key: string]: TConstructor;
+}
+
+
+export type TObject<Type extends TBaseObject> = {
+  [key in keyof Type]: InstanceType<Type[key]>
+};
+export type TObjectRead = Required<TDocument>;
+export type TObjectDelete = Required<TDocument>;
+export type TObjectCreate
+<Type extends TObject<TBaseObject>> = Partial<Type>;
+export type TObjectUpdate
+<Type extends TObject<TBaseObject>> = Required<TDocument> & Partial<Type>;
