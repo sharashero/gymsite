41df14be7cf33793323b8daa68e271ed8750ad23
sharashero <sharshar.dev@gmail.com>
2023-09-20 23:06:40 +0300

    decreased font on small screens

--- src/pages/PageLayout.tsx
+++ src/pages/PageLayout.tsx
@@ -15,7 +15,7 @@ function PageLayout({
   return (
     <>
       <header className="mb-12 mt-4 text-center">
-        <h1 className="text-5xl font-bold">{title}</h1>
+        <h1 className="text-4xl font-bold md:text-5xl">{title}</h1>
       </header>
 
       <main className="container mx-auto">
