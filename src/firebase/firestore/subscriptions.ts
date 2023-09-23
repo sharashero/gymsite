6fac31e870f3f9115eb68549714df22e8871e7b8
sharashero <sharshar.dev@gmail.com>
2023-09-23 20:52:10 +0300

    fixed collection name typo

--- src/firebase/firestore/subscriptions.ts
+++ src/firebase/firestore/subscriptions.ts
@@ -6,7 +6,6 @@ import {
   deleteDocument,
 } from "./util/crud";
 import {
-  TSubscsription,
   TSubscsriptionCreate,
   TSubscsriptionRead,
   TSubscsriptionUpdate,
@@ -17,33 +16,33 @@ import { useCollectionSnapshot } from "./util/snapshot";
 
 export function createSub(userId: string, sub: TSubscsriptionCreate) {
   return createDocument<TSubscsriptionCreate>("users", [
-    userId, "susbscriptions"
+    userId, "subscriptions"
   ], sub);
 }
 
 
 export function readSub(userId: string, sub: TSubscsriptionRead) {
   return readDocument<TSubscsriptionRead>("users", [
-    userId, "susbscriptions", sub.id
+    userId, "subscriptions", sub.id
   ]);
 }
 
 
 export function updateSub(userId: string, sub: TSubscsriptionUpdate) {
   return updateDocument<TSubscsriptionUpdate>("users", [
-    userId, "susbscriptions", sub.id
+    userId, "subscriptions", sub.id
   ], sub);
 }
 
 
 export function deleteSub(userId: string, sub: TSubscsriptionDelete) {
   return deleteDocument<TSubscsriptionDelete>("users", [
-    userId, "susbscriptions", sub.id
+    userId, "subscriptions", sub.id
   ]);
 }
 
 
 export function useSubscriptions(userId: string) {
   const path = useMemo(() => [userId, "subscriptions"], [userId]);
-  return useCollectionSnapshot<TSubscsription>("users", path);
+  return useCollectionSnapshot<TSubscsriptionUpdate>("users", path);
 }
