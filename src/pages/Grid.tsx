e2039af46d34e59ea09e4e3932b716d12070e582
sharashero <sharshar.dev@gmail.com>
2023-09-23 18:09:10 +0300

    added Grid

---
+++ src/pages/Grid.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+
+
+interface IGrid {
+  children: ReactNode;
+}
+
+
+function Grid({ children }: IGrid) {
+  return (
+    <div className="grid w-full grid-cols-1 justify-items-center gap-8 md:grid-cols-2 xl:grid-cols-3">
+      {children}
+    </div>
+  );
+}
+
+
+export default Grid;
